5db0b3d1d72ea42b7cb5b34311e622070718c734
jorget0rr3s <torresor.jt@gmail.com>
2023-07-11 18:41:15 -0500

    first comiit

---
+++ script.js
@@ -0,0 +1,39 @@
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 150
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+}
+
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    
+}
+
+fetchPokemons()
