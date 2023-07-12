967c71dc20ad95ba359bf5a7ca490f012505802c
9AndrewsEsteban8 <andrewsesteban7@gmail.com>
2023-07-11 20:24:10 -0500

    creación de tarjetas

--- script.js
+++ script.js
@@ -37,3 +37,58 @@ const createPokemonCard = (pokemon) => {
 }
 
 fetchPokemons()
+
+function generarDivs() {
+var container = document.getElementById('poke-container');
+
+for (var i = 0; i < pokemon_count; i++) {
+	// Crear elemento div con la clase "pokemon"
+	var div = document.createElement('div');
+	div.classList.add('pokemon');
+	div.style.backgroundColor = ''; // depende el tipo de pokemón
+
+	// Crear div con la clase "img-container" para la imagen
+	var imgContainer = document.createElement('div');
+	imgContainer.classList.add('img-container');
+
+	// Crear imagen
+	var img = document.createElement('img');
+	img.src = ''; // Cambia la URL de la imagen según tus necesidades
+	img.alt = ''; // Cambia el texto alternativo de la imagen según tus necesidades
+	imgContainer.appendChild(img);
+	div.appendChild(imgContainer);
+
+	// Crear div con la clase "info" para la información
+	var info = document.createElement('div');
+	info.classList.add('info');
+
+	// Crear span con la clase "number" para el número del Pokémon
+	var number = document.createElement('span');
+	number.classList.add('number');
+	number.textContent = '';
+	info.appendChild(number);
+
+	// Crear h3 con la clase "name" para el nombre del Pokémon
+	var name = document.createElement('h3');
+	name.classList.add('name');
+	name.textContent = '';
+	info.appendChild(name);
+
+	// Crear small con la clase "type" para el tipo del Pokémon
+	var type = document.createElement('small');
+	type.classList.add('type');
+	type.textContent = 'Type: ';
+	var typeSpan = document.createElement('span');
+	typeSpan.textContent = ''; // Cambia el tipo del Pokémon según tus necesidades
+	type.appendChild(typeSpan);
+	info.appendChild(type);
+
+	div.appendChild(info);
+
+	// Agregar el div al contenedor
+	container.appendChild(div);
+}
+}
+
+generarDivs();
+
